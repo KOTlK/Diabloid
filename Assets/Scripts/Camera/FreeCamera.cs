24ff0da0b19ded10d42ba45dba5fd5bd287d123c
Konstantin Osintsev <rojuty@gmail.com>
2021-07-17 15:51:51 +0700

    added camera controls

--- Assets/Scripts/Camera/FreeCamera.cs
+++ Assets/Scripts/Camera/FreeCamera.cs
@@ -1,8 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game.Storages;
+using Game.PlayerInput;
 
 public class FreeCamera : MonoBehaviour
 {
+    private float _speed = 40f;
+    private const float _rotationAngle = 20f;
+    private const float _minCameraSize = 10f;
+    private const float _maxCameraSize = 20f;
+    private const float _cameraZoomMultiplyer = -7; //negative to zoomin on mousewheelup, positive to zoomin on mousewheeldown
+    private const float _defaultSpeed = 40;
+    private const float _fastSpeed = 80;
+    private Vector3 _targetVelocity;
+
+    private Camera _camera;
+    private Rigidbody _cameraRigidbody;
+
+    private CameraInput _input;
+
+    private void Awake()
+    {
+        _input = new CameraInput();
+    }
+
+    private void Start()
+    {
+        _camera = Storages.GameData.Camera.GetFirstItem();
+        _cameraRigidbody = _camera.GetComponent<Rigidbody>();
+        _camera.orthographicSize = _minCameraSize;
+        _camera.transform.localEulerAngles = (new Vector3(_rotationAngle, 0, 0));
+    }
+
+    private void Update()
+    {
+        _input.UpdateInput();
+        ZoomCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCamera();
+        SpeedUp();
+
+
+    }
+
+    private void MoveCamera()
+    {
+        _targetVelocity.x = _speed * _input.Movement.x;
+        _targetVelocity.z = _speed * _input.Movement.y;
+        _targetVelocity.y = 0;
+        _cameraRigidbody.velocity = _targetVelocity;
+    }
+
+    private void ZoomCamera()
+    {
+        var newSize = _camera.orthographicSize + _input.MouseScrollDelta * _cameraZoomMultiplyer;
+        _camera.orthographicSize = Mathf.Clamp(newSize, _minCameraSize, _maxCameraSize);
+    }
+
+    private void SpeedUp()
+    {
+        if (_input.ShiftHolding)
+        {
+            _speed = _fastSpeed;
+        } else
+        {
+            _speed = _defaultSpeed;
+        }
+    }
+
+
+
     
 }
---
+++ Assets/Scripts/Input/CameraInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.PlayerInput
+{
+    public class CameraInput
+    {
+        public Vector2 Movement { get; private set; }
+        public float MouseScrollDelta { get; private set; }
+        public bool ShiftHolding { get; private set; }
+
+
+        public void UpdateInput()
+        {
+            Movement = UpdateMovement();
+            MouseScrollDelta = ReadMouseWheel();
+            ShiftHolding = ReadShiftButton();
+        }
+        private Vector3 UpdateMovement()
+        {
+            Vector2 temp;
+            temp.x = Input.GetAxisRaw("Horizontal");
+            temp.y = Input.GetAxisRaw("Vertical");
+            return temp;
+        }
+
+        private float ReadMouseWheel()
+        {
+            return Input.GetAxisRaw("Mouse ScrollWheel");
+        }
+
+        private bool ReadShiftButton()
+        {
+            return Input.GetKey(KeyCode.LeftShift);
+        }
+    }
+}
